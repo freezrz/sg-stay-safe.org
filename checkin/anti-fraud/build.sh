862dbde7076f2167f38ad0f5d7d1859c2854c26c
Huang Luohua <huanglh@sea.com>
2022-04-22 15:04:22 +0800

    dummy update

--- checkin/anti-fraud/build.sh
+++ checkin/anti-fraud/build.sh
@@ -2,5 +2,5 @@ rm anti-fraud-bin
 GOOS=linux GOARCH=amd64 go build -o anti-fraud-bin
 
 rm anti-fraud-bin.zip
-zip anti-fraud-bin.zip anti-fraud-bin
+zip anti-fraud-bin.zip anti-fraud-bin # dummy update
 
