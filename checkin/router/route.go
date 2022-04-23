0b56edf89f00d61f6a7073b89bb46ec617dd3af6
Huang Luohua <huanglh@sea.com>
2022-04-23 20:24:58 +0800

    add ping pong page

--- checkin/router/route.go
+++ checkin/router/route.go
@@ -18,10 +18,9 @@ import (
 func main() {
 	log.Println("checkin in router service...")
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
 
 	router.POST("/", route)
-	router.GET("/welcome", welcome)
+	router.GET("/ping", welcome)
 
 	router.Run(fmt.Sprintf(":%s", config.AWSForwardPort))
 }
@@ -44,7 +43,7 @@ func route(c *gin.Context) {
 
 func welcome(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"Welcome": "Safe Check-in Platform",
+		"pong": "Safe Check-in Platform",
 	})
 }
 
